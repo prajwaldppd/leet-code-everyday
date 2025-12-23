ff822057fd56030af7a14b9a0139bc27bc01cdbc
prajwaldppd <123709461+prajwaldppd@users.noreply.github.com>
2025-12-24 00:33:13 +0530

    Time: 34 ms (42.87%) | Memory: 94.9 MB (18.72%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
@@ -1,43 +1,26 @@
 class Solution {
-    public int bs(int target_idx, int k, int[] nums, long[] ps){
-        int l=0, r=target_idx;
-        int bestidx=target_idx;
-
-        while(l<=r){
-            int mid = l+(r-l)/2;
-
-            long count = target_idx-mid+1;
-            long requiredsum= count * nums[target_idx];
-            long cursum=ps[target_idx]-(mid>0?ps[mid-1]:0);
-            long ops=requiredsum-cursum;
-
-            if(ops>k){
-                l=mid+1;
-            }else{
-                bestidx=mid;
-                r=mid-1;
-            }
-        }
-
-        return target_idx-bestidx+1;
-    }
-
     public int maxFrequency(int[] nums, int k) {
         Arrays.sort(nums);
+
         int n = nums.length;
-        
-        long [] ps = new long[n];
-        ps[0]=nums[0];
+        int res=0;
 
-        for(int i=1;i<n;i++){
-            ps[i]=ps[i-1]+nums[i];
+        int l=0;
+        long cursum = 0;
+
+        for(int r=0; r<n; r++){
+
+            long target= nums[r];
+            cursum=cursum+nums[r];
+
+            if((r-l+1) * target -cursum >k){
+                cursum=cursum-nums[l];
+                l++;
+            }
+            res = Math.max(res, r-l+1);
+
         }
 
-        int freq=1;
-        for(int i=0;i<n;i++){
-            freq=Math.max(freq,bs(i, k, nums, ps));
-        }
-
-        return freq;
+        return res;
     }
 }
