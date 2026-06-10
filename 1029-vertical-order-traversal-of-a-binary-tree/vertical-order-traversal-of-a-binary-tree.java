23954a3687fd3c59f36bee361a02e27202ec8af1
prajwaldppd <123709461+prajwaldppd@users.noreply.github.com>
2026-06-10 10:55:39 +0530

    Time: 4 ms (58.06%) | Memory: 43.9 MB (74.60%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -14,63 +14,55 @@
  * }
  */
 class Solution {
-    class Tuple {
+    class Tuple{
         TreeNode node;
         int row;
         int col;
-        Tuple(TreeNode node, int row, int col) {
-            this.node = node;
-            this.row = row;
-            this.col = col;
+
+        Tuple(TreeNode n, int r, int c){
+            this.node =n;
+            this.row=r;
+            this.col=c;
         }
     }
 
-    class Data {
+    class Data{
+        int val;
         int row;
         int col;
-        int val;
-        Data(int row, int col, int val) {
-            this.row = row;
-            this.col = col;
-            this.val = val;
+        Data(int v, int r, int c){
+            this.val=v;
+            this.row=r;
+            this.col=c;
         }
     }
 
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-
         List<Data> nodes = new ArrayList<>();
-
         Queue<Tuple> q = new LinkedList<>();
+        q.offer(new Tuple(root,0,0));
 
-        q.offer(new Tuple(root, 0, 0));
-
-        while(!q.isEmpty()) {
-            Tuple curr = q.poll();
-            TreeNode node = curr.node;
-            int row = curr.row;
-            int col = curr.col;
-
-            nodes.add(new Data(row, col, node.val));
-
-            if(node.left != null)  q.offer(new Tuple(node.left,row+1,col-1));
-            if(node.right != null) q.offer(new Tuple(node.right,row+1,col+1));
+        while(!q.isEmpty()){
+            Tuple cur= q.poll();
+            if(cur.node.left!=null) q.offer(new Tuple(cur.node.left,cur.row+1,cur.col-1));
+            if(cur.node.right!=null) q.offer(new Tuple(cur.node.right,cur.row+1,cur.col+1));
+            nodes.add(new Data(cur.node.val,cur.row,cur.col));
         }
 
-        Collections.sort(nodes, (a, b) -> {
-            if(a.col != b.col)return Integer.compare(a.col, b.col);
-            if(a.row != b.row) return Integer.compare(a.row, b.row);
-            return Integer.compare(a.val, b.val);
+        Collections.sort(nodes,(a,b)->{
+            if(a.col!=b.col) return Integer.compare(a.col,b.col);
+            if(a.row!=b.row) return Integer.compare(a.row,b.row);
+            return Integer.compare(a.val,b.val);
         });
 
         List<List<Integer>> res = new ArrayList<>();
-        Integer prevCol = null;
-        for(Data d : nodes) {
-
-            if(prevCol == null || d.col != prevCol) {
+        Integer prev = null;
+        for(Data d: nodes){
+            if(prev==null || d.col!=prev){
                 res.add(new ArrayList<>());
-                prevCol = d.col;
+                prev=d.col;
             }
-            res.get(res.size() - 1).add(d.val);
+            res.get(res.size()-1).add(d.val);
         }
         return res;
     }
