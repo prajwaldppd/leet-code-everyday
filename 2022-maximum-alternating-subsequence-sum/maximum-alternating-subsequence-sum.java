17821b6997474ec8fb8eae889e0102973a845ead
prajwaldppd <123709461+prajwaldppd@users.noreply.github.com>
2026-06-25 10:55:39 +0530

    Time: 29 ms (66.62%) | Memory: 104.6 MB (54.68%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.java
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.java
@@ -1,24 +1,15 @@
 class Solution {
-    int n;
-    public long solve(int idx, int[] nums, boolean flag, long[][]dp){
-        if(idx>=n) return 0;
-        // skip 
-        int fidx= flag?1:0;
-        if(dp[idx][fidx]!=-1) return dp[idx][fidx];
-        long skip = solve(idx+1,nums,flag,dp);
-
-        // take if flag is false then we do minus else plus 
-        int val = nums[idx];
-        if(flag==false) val=-val;
-        long take = val+solve(idx+1,nums,!flag,dp);
-        return dp[idx][fidx]=Math.max(skip,take);
-    }
     public long maxAlternatingSum(int[] nums) {
-        n = nums.length;
-        long[][] dp = new long[n+1][2];
-        for(long[] row:dp){
-            Arrays.fill(row,-1);
+        int n = nums.length;
+        long[][] t = new long[n+1][2];
+        for(long[] row:t){
+            Arrays.fill(row,0);
         }
-        return solve(0,nums,true,dp); // true : + 
+
+        for(int i=1;i<n+1;i++){
+            t[i][0] = Math.max(t[i-1][1]-nums[i-1],t[i-1][0]);
+            t[i][1]=Math.max(t[i-1][0]+nums[i-1],t[i-1][1]);
+        }
+        return Math.max(t[n][0],t[n][1]);
     }
 }
