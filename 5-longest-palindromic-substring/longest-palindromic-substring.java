f3725af7fee236fc6f4734546ffda536c9c98281
prajwaldppd <123709461+prajwaldppd@users.noreply.github.com>
2026-07-08 12:50:17 +0530

    Time: 171 ms (21.55%) | Memory: 48.5 MB (5.53%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,26 +1,24 @@
 class Solution {
-    int len,idx;
-    int [][] t= new int [1001][1001];
+    int len, idx;
     public String longestPalindrome(String s) {
         int n = s.length();
-        for(int[] row:t) Arrays.fill(row,-1);
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if (solve(s,i,j)==1){
-                    if(j-i+1 > len){
+        boolean[][] t = new boolean[n+1][n+1];
+        for(int l=1;l<=n;l++){
+            for(int i=0;i+l-1<n;i++){
+                int j = i+l-1;
+                if(i==j) t[i][j]=true;
+                else if(i+1==j) t[i][j]=s.charAt(i)==s.charAt(j);
+                else t[i][j]=(s.charAt(i)==s.charAt(j)) && t[i+1][j-1];
+
+                if(t[i][j]){
+                    if(j-i+1>len){
                         len=j-i+1;
                         idx=i;
                     }
                 }
             }
         }
-        return s.substring(idx,idx+len);
-    }
 
-    public int solve(String s, int i, int j){
-        if(i>=j) return 1;
-        if(t[i][j]!=-1) return t[i][j];
-        if(s.charAt(i)==s.charAt(j)) return t[i][j]= solve(s,i+1,j-1);
-        return 0;
+        return s.substring(idx,idx+len);
     }
 }
