e286dd7787dad5b687288d954cfa78a620f4f4ad
prajwaldppd <123709461+prajwaldppd@users.noreply.github.com>
2026-07-15 12:53:54 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (64.31%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -3,17 +3,16 @@ class Solution {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
         int[][] t = new int[m+1][n+1];
-        for(int[] row:t) Arrays.fill(row,-1);
-        return solve(0,0,obstacleGrid,t,m,n);
-    }
 
-    private int solve(int i, int j, int[][] ob, int[][] t, int m, int n){
-        if(i<0 || i>=m || j<0 || j>=n) return 0;
-        if(ob[i][j]==1) return 0;
-        if(i==m-1 && j==n-1) return 1;
-        if(t[i][j]!=-1) return t[i][j];
-        int right = solve(i,j+1,ob,t,m,n);
-        int down = solve(i+1,j,ob,t,m,n);
-        return t[i][j]=right+down;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(obstacleGrid[i][j]==1) t[i][j]=0;
+                else if(i==0 && j==0) t[i][j]=1;
+                else if(j==0) t[i][j]=t[i-1][j];
+                else if (i==0) t[i][j]=t[i][j-1];
+                else t[i][j]=t[i-1][j]+t[i][j-1];
+            }
+        }
+        return t[m-1][n-1];
     }
 }
