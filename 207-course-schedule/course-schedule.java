5fd9160cc4a2908b6fec8bf8badb3c6cf299adc5
prajwaldppd <123709461+prajwaldppd@users.noreply.github.com>
2026-07-31 15:59:12 +0530

    Time: 8 ms (24.57%) | Memory: 47 MB (79.06%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,38 +1,35 @@
 class Solution {
     public boolean canFinish(int n, int[][] prerequisites) {
         List<List<Integer>> adj = new ArrayList<>();
-        for(int i=0;i<n;i++){
+        for(int i=0; i<n;i++){
             adj.add(new ArrayList<>());
         }
 
         for(int[] e:prerequisites){
-            adj.get(e[0]).add(e[1]);
+            adj.get(e[1]).add(e[0]);
         }
 
-        boolean[] vis = new boolean[n];
-        boolean[] inRec= new boolean[n];
-
+        int[] indegree = new int[n];
         for(int i=0;i<n;i++){
-            if(!vis[i] && cycleDFS(adj,i,vis,inRec)){
-                return false;
+            for(int v:adj.get(i)){
+                indegree[v]++;
             }
         }
-        return true;
-    }
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0) q.offer(i);
+        }
 
-    private boolean cycleDFS( List<List<Integer>> adj, int u, boolean[] vis, boolean[] inRec){
-        vis[u]=true;
-        inRec[u]=true;
-
-        for(int v:adj.get(u)){
-            if(!vis[v] && cycleDFS(adj,v,vis,inRec)){
-                return true;
-            }else if(inRec[v]){
-                return true;
+        int count=0;
+        while(!q.isEmpty()){
+            int node = q.poll();
+            count++;
+            for(int v:adj.get(node)){
+                indegree[v]--;
+                if(indegree[v]==0) q.offer(v);
             }
         }
 
-        inRec[u]=false;
-        return false;
+        return count==n;
     }
 }
