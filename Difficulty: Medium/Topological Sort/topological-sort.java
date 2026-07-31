6d8dac8025bda3f5dcbd5fad51a05e455fb202b0
prajwaldppd <123709461+prajwaldppd@users.noreply.github.com>
2026-07-31 15:25:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological Sort/topological-sort.java
+++ Difficulty: Medium/Topological Sort/topological-sort.java
@@ -1,37 +1,41 @@
 class Solution {
     public ArrayList<Integer> topoSort(int V, int[][] edges) {
         // code here
-         List<List<Integer>> adj = new ArrayList<>();
-         
-         for(int i=0;i<V;i++){
-             adj.add(new ArrayList<>());
-         }
-         
-         for(int[] e:edges){
-             adj.get(e[0]).add(e[1]);
-         }
-         
-         boolean[] vis = new boolean[V];
-         Stack<Integer> st = new Stack<>();
-         
-        for(int i=0;i<V;i++){
-            if(!vis[i]){
-                DFS(adj,i,vis,st);
-            }
-        }
-         
-         ArrayList<Integer> ans = new ArrayList<>();
-         while(!st.isEmpty()){
-             ans.add(st.pop());
-         }
-        return ans;
-    }
-    
-    private void DFS(List<List<Integer>> adj, int u, boolean[] vis, Stack<Integer> st){
-        vis[u]=true;
-        for(int v:adj.get(u)){
-            if(!vis[v]) DFS(adj,v,vis,st);
-        }
-        st.push(u);
+        ArrayList<Integer> ans = new ArrayList<>();
+       List<List<Integer>> adj = new ArrayList<>();
+       for(int i=0;i<V;i++){
+           adj.add(new ArrayList<>());
+       }
+       
+       for(int[] e:edges){
+           adj.get(e[0]).add(e[1]);
+       }
+       
+       int[] indegree = new int[V];
+       
+       Arrays.fill(indegree,0);
+       
+       for(int u=0;u<V;u++){
+           for(int v:adj.get(u)){
+               indegree[v]++;
+           }
+       }
+       
+       Queue<Integer> q = new LinkedList<>();
+       for(int i=0;i<V;i++){
+           if(indegree[i]==0) q.offer(i);
+       }
+       
+       // now bfs code.
+       while(!q.isEmpty()){
+           int node = q.poll();
+           ans.add(node);
+           for(int v:adj.get(node)){
+               indegree[v]--;
+               if(indegree[v]==0) q.offer(v);
+           }
+       }
+       
+       return ans;
     }
 }
